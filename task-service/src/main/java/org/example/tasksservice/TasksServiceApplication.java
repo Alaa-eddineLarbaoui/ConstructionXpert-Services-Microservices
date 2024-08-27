51d33114f4a71274828afc30d5d280c517ffd271
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-08-27 18:17:51 +0100

    J'ai ajouté les dépendances nécessaires ainsi qu'une annotation pour intégrer Eureka dans le service de gestion des tâches.

--- task-service/src/main/java/org/example/tasksservice/TasksServiceApplication.java
+++ task-service/src/main/java/org/example/tasksservice/TasksServiceApplication.java
@@ -2,8 +2,10 @@ package org.example.tasksservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TasksServiceApplication {
 
     public static void main(String[] args) {
