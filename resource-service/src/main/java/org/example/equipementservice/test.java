7af7ba71c427c8d1678bc3efc470757986f9ee79
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-08-27 23:51:53 +0100

    j'ai ajoute des dependencies et une annotation pour utiliser eureka dans le service de resource

--- resource-service/src/main/java/org/example/equipementservice/test.java
+++
@@ -1,4 +0,0 @@
-package org.example.equipementservice;
-
-public class test {
-}
--- resource-service/src/main/java/org/example/equipementservice/testRepo.java
+++
@@ -1,6 +0,0 @@
-package org.example.equipementservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface testRepo extends JpaRepository<test,Long> {
-}
