6d6fd11356e4ce41cbe06b2354064d59591830fa
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-08-29 16:55:08 +0100

    j'ai cree le controller de projet ressource

---
+++ resource-service/src/main/java/org/example/resourceservice/controller/ResourceController.java
@@ -0,0 +1,46 @@
+package org.example.resourceservice.controller;
+
+import org.example.resourceservice.model.Resource;
+import org.example.resourceservice.service.ResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/resource")
+public class ResourceController {
+
+    @Autowired
+    private ResourceService resourceService;
+
+    @GetMapping("/getAll")
+    public List<Resource> showAllResources() {
+        return resourceService.showResources();
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Resource> getResourceById(@PathVariable Long id) {
+        Resource resource = resourceService.findResource(id);
+        return ResponseEntity.ok(resource);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Resource> createResource(@RequestBody Resource resource) {
+        Resource savedResource = resourceService.saveResource(resource);
+        return ResponseEntity.ok(savedResource);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Resource> updateResource(@PathVariable Long id, @RequestBody Resource updatedResource) {
+        Resource updated = resourceService.updateResource(updatedResource, id);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteResource(@PathVariable Long id) {
+        resourceService.deleteResource(id);
+        return ResponseEntity.noContent().build();
+    }
+}
