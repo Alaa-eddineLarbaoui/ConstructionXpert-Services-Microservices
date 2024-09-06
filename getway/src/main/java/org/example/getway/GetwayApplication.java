4dd4b00879e9b62e202ee27bd6df2257ed314b04
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-06 18:17:57 +0100

    J'ai ajouté les dépendances de sécurité pour tous les services

--- getway/src/main/java/org/example/getway/GetwayApplication.java
+++ getway/src/main/java/org/example/getway/GetwayApplication.java
@@ -2,8 +2,12 @@ package org.example.getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableWebFlux
 public class GetwayApplication {
 
     public static void main(String[] args) {
